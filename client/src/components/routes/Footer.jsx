abbfb13131a670361126536a09e035e8275608f6
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-07-12 16:47:23 +0530

    fix: update footer date to 2026.07.12

--- client/src/components/routes/Footer.jsx
+++ client/src/components/routes/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section className="sticky -z-10  bg-(--bg-dark) bottom-0 left-0 h-16 w-full flex justify-around items-center ">
       <p className="lg:text-sm md:text-sm text-xs">
-        UPDATED <span className="ml-2">2026.07.11</span>
+        UPDATED <span className="ml-2">2026.07.12</span>
       </p>
 
       <p className="lg:text-sm md:text-sm text-xs">
