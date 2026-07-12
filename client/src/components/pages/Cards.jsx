951b1ec946bb68fcd8cf4e8bc19fbc0c240e36e1
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-07-12 16:27:14 +0530

    refactor: improve animation properties and transition effects in Cards component

--- client/src/components/pages/Cards.jsx
+++ client/src/components/pages/Cards.jsx
@@ -5,13 +5,13 @@ const Cards = () => {
   return (
     <div className="bg-(--bg)   overflow-hidden min-h-screen items-center   w-full p-5 flex flex-col">
       <motion.div
-        initial={{ opacity: 0, scale: 0.8, y: 80, filter: "blur(6px)" }}
-        whileInView={{ opacity: 1, scale: 1, y: 0, filter: "blur(0px)" }}
+        initial={{ opacity: 0, scale: 0.9, y: 80 }}
+        whileInView={{ opacity: 1, scale: 1, y: 0 }}
         transition={{
           type: "spring",
-          stiffness: 80,
-          damping: 14,
-          delay: 0.1,
+          stiffness: 200,
+          damping: 35,
+          mass: 1,
         }}
         viewport={{ amount: 0.5, margin: "80px 0px 0px 0px" }}
         className="flex  justify-center  mt-5  mb- items-center font-bold leading-none text-[15vw] md:text-[10vw] lg:text-[10vw]"
